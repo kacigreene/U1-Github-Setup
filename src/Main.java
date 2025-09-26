b9263063417dd896582d8d7c1bbbe96b81004385
KaciGreene <kacig@nycstudents.net>
2025-09-26 14:44:49 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         System.out.println("------------------------------------------------------------------");
         System.out.println("| Note! Do not use any symbols when inputting data! (no $ or %) |");
         System.out.println("------------------------------------------------------------------");
@@ -19,16 +18,20 @@ public class Main {
         System.out.println();
         System.out.println("-----------------------------------------");
         double tipAmount = (double)((int)(((billTotal * ((double) tipPercent / 100)) * 100) + 0.5)) / 100;
-        System.out.println("| Total tip amount: $" + tipAmount + "             |");
+        System.out.print("| Total tip amount: ");
+        System.out.printf("$%.2f\n", tipAmount);
         System.out.println("-----------------------------------------");
-        double totalWithTip = billTotal + tipAmount;
-        System.out.println("| Total bill cost: $" + totalWithTip + "            |");
+        double totalWithTip = (billTotal / 100 + tipAmount / 100) * 100;
+        System.out.print("| Total bill cost: ");
+        System.out.printf("$%.2f\n", totalWithTip);
         System.out.println("-----------------------------------------");
         double tipPerson = (double)((int)(((tipAmount / numPeople) * 100) + 0.5)) / 100;
         double billPerson = (double)((int)(((totalWithTip / numPeople) * 100) + 0.5)) / 100;
-        System.out.println("| Tip per person: $" + tipPerson + "                |");
+        System.out.print("| Tip per person: ");
+        System.out.printf("$%.2f\n", tipPerson);
         System.out.println("-----------------------------------------");
-        System.out.println("| Total bill per person: $" + billPerson + "        |");
+        System.out.print("| Total bill per person: ");
+        System.out.printf("$%.2f\n", billPerson);
         System.out.println("-----------------------------------------");
     }
 }
