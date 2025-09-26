7057840b3e16bab5ce67309bd376e49cc8b7fcb1
KaciGreene <kacig@nycstudents.net>
2025-09-26 14:09:39 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("| Note! Do not use any symbols when inputting data! (no $ or %) |");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("Enter your Bill($): ");
+        double billTotal = scan.nextDouble();
+        scan.nextLine();
+        System.out.println("Tip percent: ");
+        int tipPercent = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Number of people: ");
+        int numPeople = scan.nextInt();
+        scan.nextLine();
+        System.out.println();
+        System.out.println("-----------------------------------------");
+        double tipAmount = (double)((int)(((billTotal * ((double) tipPercent / 100)) * 100) + 0.5)) / 100;
+        System.out.println("| Total tip amount: $" + tipAmount + "             |");
+        System.out.println("-----------------------------------------");
+        double totalWithTip = billTotal + tipAmount;
+        System.out.println("| Total bill cost: $" + totalWithTip + "            |");
+        System.out.println("-----------------------------------------");
+        double tipPerson = (double)((int)(((tipAmount / numPeople) * 100) + 0.5)) / 100;
+        double billPerson = (double)((int)(((totalWithTip / numPeople) * 100) + 0.5)) / 100;
+        System.out.println("| Tip per person: $" + tipPerson + "                |");
+        System.out.println("-----------------------------------------");
+        System.out.println("| Total bill per person: $" + billPerson + "        |");
+        System.out.println("-----------------------------------------");
     }
 }
