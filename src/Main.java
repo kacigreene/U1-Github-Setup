304d179d750b5e664950d13b15ef637b84d5c365
kacigreene <kacig@nycstudents.net>
2025-09-29 17:44:28 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,32 @@ public class Main {
         System.out.println("------------------------------------------------------------------");
         System.out.println("| Note! Do not use any symbols when inputting data! (no $ or %) |");
         System.out.println("------------------------------------------------------------------");
-        System.out.println("Enter your Bill($): ");
-        double billTotal = scan.nextDouble();
+        System.out.println("Enter your Bill($): ");                                        //getting user input
+        double billTotal = scan.nextDouble();                                              //storing input as variable
         scan.nextLine();
-        System.out.println("Tip percent: ");
-        int tipPercent = scan.nextInt();
+        System.out.println("Tip percent: ");                                               //getting user input
+        int tipPercent = scan.nextInt();                                                   //storing input as variable
         scan.nextLine();
-        System.out.println("Number of people: ");
-        int numPeople = scan.nextInt();
+        System.out.println("Number of people: ");                                          //getting user input
+        int numPeople = scan.nextInt();                                                    //storing input as variable
         scan.nextLine();
         System.out.println();
         System.out.println("-----------------------------------------");
         double tipAmount = (double)((int)(((billTotal * ((double) tipPercent / 100)) * 100) + 0.5)) / 100;
-        System.out.print("| Total tip amount: ");
-        System.out.printf("$%.2f\n", tipAmount);
+        System.out.print("| Total tip amount: ");                                          //output
+        System.out.printf("$%.2f\n", tipAmount);                                           //rounding value to 2 decimal places
         System.out.println("-----------------------------------------");
         double totalWithTip = (billTotal / 100 + tipAmount / 100) * 100;
-        System.out.print("| Total bill cost: ");
-        System.out.printf("$%.2f\n", totalWithTip);
+        System.out.print("| Total bill cost: ");                                           //output
+        System.out.printf("$%.2f\n", totalWithTip);                                        //rounding value to 2 decimal places
         System.out.println("-----------------------------------------");
         double tipPerson = (double)((int)(((tipAmount / numPeople) * 100) + 0.5)) / 100;
         double billPerson = (double)((int)(((totalWithTip / numPeople) * 100) + 0.5)) / 100;
-        System.out.print("| Tip per person: ");
-        System.out.printf("$%.2f\n", tipPerson);
+        System.out.print("| Tip per person: ");                                            //output
+        System.out.printf("$%.2f\n", tipPerson);                                           //rounding value to 2 decimal places
         System.out.println("-----------------------------------------");
-        System.out.print("| Total bill per person: ");
-        System.out.printf("$%.2f\n", billPerson);
+        System.out.print("| Total bill per person: ");                                     //output
+        System.out.printf("$%.2f\n", billPerson);                                          //rounding value to 2 decimal places
         System.out.println("-----------------------------------------");
     }
 }
